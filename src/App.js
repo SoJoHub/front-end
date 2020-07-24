618d0dadebb9c3b7985283986c41f5d459875c9d
ccedacero <cristian.cedacero@gmail.com>
2020-07-24 15:00:02 -0400

    initial nav try

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./App.css";
 import CssBaseline from "@material-ui/core/CssBaseline";
-
+import Navbar from "./Components/Nav/Navbar";
 export default function MyApp() {
   return (
     <React.Fragment>
       <CssBaseline />
-      <div className="App"></div>;
+      <div className="App">{<Navbar />}</div>;
     </React.Fragment>
   );
 }
---
+++ src/Components/Nav/Navbar.js
@@ -0,0 +1,22 @@
+import react from "react";
+
+function Navbar() {
+  <AppBar position="static">
+    <Toolbar>
+      <IconButton
+        edge="start"
+        className={classes.menuButton}
+        color="inherit"
+        aria-label="menu"
+      >
+        <MenuIcon />
+      </IconButton>
+      <Typography variant="h6" className={classes.title}>
+        News
+      </Typography>
+      <Button color="inherit">Login</Button>
+    </Toolbar>
+  </AppBar>;
+}
+
+export default Navbar;
