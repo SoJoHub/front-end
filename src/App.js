a708fe5126bcb582e0a6206b5918c1f6e8b7a76a
bashir2017 <bashiralhanshali@yahoo.com>
2020-07-25 14:34:33 -0400

    fixed conflicts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./App.css";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import Navbar from "./components/Nav/Navbar";
-import ApplicationsContainer from "./containers/ApplicationsContainer";
-import SignUp from './components/Signup/SignUp'
+import Navbar from "./Components/Nav/Navbar";
+import ApplicationsContainer from "./Containers/ApplicationsContainer";
+import SignUp from './Components/Signup/SignUp'
 
 export default function App() {
   return (
